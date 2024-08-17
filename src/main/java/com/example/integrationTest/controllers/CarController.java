ac751013c921e58f833d821f5c93ce3c167c0614
lutencjusz <lutencjusz@gmail.com>
2024-08-17 09:05:26 +0200

    patch hateoas

--- src/main/java/com/example/integrationTest/controllers/CarController.java
+++ src/main/java/com/example/integrationTest/controllers/CarController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/cars")
@@ -28,19 +28,17 @@ public class CarController {
 
     @GetMapping
     public CollectionModel<EntityModel<Car>> getAllCars() {
-        List<Car> cars = carService.getAllCars();
-        List<EntityModel<Car>> halCars = cars.stream()
-                .map(car -> EntityModel.of(car, linkTo(CarController.class).slash(car.getId()).withSelfRel()))
-                .collect(Collectors.toList());
-        Link link = linkTo(CarController.class).withSelfRel();
-        return CollectionModel.of(halCars, link);
+        List<EntityModel<Car>> cars = carService.getAllCars().stream()
+                .map(car -> EntityModel.of(car, linkTo(methodOn(CarController.class).getCarById(car.getId())).withSelfRel()))
+                .toList();
+        return CollectionModel.of(cars, linkTo(methodOn(CarController.class).getAllCars()).withSelfRel());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getCarById(@PathVariable("id") Long id) {
+    public ResponseEntity<EntityModel<Car>> getCarById(@PathVariable("id") Long id) {
         Link link = linkTo(CarController.class).slash(id).withSelfRel();
         Optional<Car> carById = Optional.ofNullable(carService.getCarById(id));
-        return carById.map(car -> ResponseEntity.ok().body(car.add(link)))
+        return carById.map(car -> ResponseEntity.ok().body(EntityModel.of(car, link)))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
     @PostMapping
